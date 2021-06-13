556238cb28d0c2d2e8dd135cecaeaed4a8c18542
ADKauth <adamkauth@gmail.com>
2021-06-13 14:47:54 -0700

    Added sample scene to show off picking up items and "using" items to complete "puzzle"

---
+++ Scripts/CrabGuy.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class CrabGuy : Obstacle
+{
+    // [Export] String itemNeeded;
+    // [Export] String completionText = "";  
+    // [Export] String failureText = "";
+    // [Export] String alreadyCompletedText = "";
+    // Sprite startSprite;
+    // Sprite completeSprite;
+    //PlayerMovementAllDirections steve;
+
+    //Boolean completed = false;
+
+    // private void completeTask()
+    // {
+    //     completeSprite.Visible = true;
+    //     startSprite.Visible = false;
+    //     completed = true;
+    // }
+}
