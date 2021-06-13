70c5cecb3189ad481d36d5caee7adaa36d8b1a4d
Andrew Rakos <pXn.Tilde@protonmail.com>
2021-06-13 11:26:49 -0700

    Test animation xfade trigger

--- Scripts/Andrew Test/Animator.cs
+++ Scripts/Andrew Test/Animator.cs
@@ -1,21 +1,23 @@
 using Godot;
 using System;
 
-public class Animator : Node
+public class Animator : AnimationPlayer
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public void OnDiveButtonDown()
     {
-        
+        if (IsPlaying() is false)
+        {
+            CurrentAnimation = "Down";
+            Play();
+        }
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public void OnRiseButtonDown()
+    {
+        if (IsPlaying() is false)
+        {
+            CurrentAnimation = "Up";
+            Play();
+        }
+    }
 }
