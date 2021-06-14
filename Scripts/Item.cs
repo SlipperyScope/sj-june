cad5b17e9b5401e2bb72a4380c2468c1656bc28d
ADKauth <adamkauth@gmail.com>
2021-06-13 21:36:59 -0700

    Fix terrible code formatting and import files

--- Scripts/Item.cs
+++ Scripts/Item.cs
@@ -12,17 +12,6 @@ public class Item : Clickable
         itemSprite = GetNode<Sprite>("ItemSprite");
     }
 
-    // public override void _InputEvent(Godot.Object viewport, InputEvent @event, int shapeIdx)
-    // {
-	// 	if (@event.IsActionPressed("click") && steve.Position.DistanceTo(Position) < 300)
-	// 	{
-    //         steve.grabItem(itemName,  itemSprite.Texture);
-    //         InputPickable = false;
-    //         itemSprite.Visible = false;
-    //         GetNode<CollisionShape2D>("ItemCollider").Disabled = true;
-    //     }
-    // }
-
     public override void interact()
     {
         getSteve().grabItem(itemName,  itemSprite.Texture);
