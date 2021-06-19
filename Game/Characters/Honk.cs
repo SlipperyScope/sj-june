232402fa556f305a370bcf74ab56f998f6bea5ac
Andrew Rakos <pXn.Tilde@protonmail.com>
2021-06-18 18:56:11 -0700

    📯

---
+++ Game/Characters/Honk.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public class Honk : Button
+{
+    private AudioStreamPlayer Honker;
+    public override void _Ready()
+    {
+        Honker = FindNode("Honker") as AudioStreamPlayer;
+    }
+
+    public void _on_Button_pressed()
+    {
+        Honker.Play();
+    }
+}
