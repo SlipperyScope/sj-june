ca26cda6c8f7f8e643e57fcbd7a4f35b480a14ca
ADKauth <adamkauth@gmail.com>
2021-06-20 12:54:57 -0700

    Added DialogueTrigger and placed them in the places noted in the background art

--- Scripts/Clickable.cs
+++ Scripts/Clickable.cs
@@ -4,57 +4,57 @@ using System.Collections.Generic;
 
 public class Clickable : StaticBody2D
 {
-    [Export] String name = "Something you can click on";
-    Player steve;
-    Boolean completed = false;
-    int activationDistance = 300;
+	[Export] String name = "Something you can click on";
+	Player steve;
+	Boolean completed = false;
+	int activationDistance = 300;
 
-    public override void _Ready()
-    {
-        InputPickable = true;
-        this.Connect("mouse_entered", this, nameof(_onMouseEntered));
-        this.Connect("mouse_exited", this, nameof(_onMouseExited));
-    }
+	public override void _Ready()
+	{
+		InputPickable = true;
+		this.Connect("mouse_entered", this, nameof(_onMouseEntered));
+		this.Connect("mouse_exited", this, nameof(_onMouseExited));
+	}
 
-    public void setSteve(Player theSteve)
-    {
-        steve = theSteve;
-    }
-    public override void _InputEvent(Godot.Object viewport, InputEvent @event, int shapeIdx)
-    {
+	public void setSteve(Player theSteve)
+	{
+		steve = theSteve;
+	}
+	public override void _InputEvent(Godot.Object viewport, InputEvent @event, int shapeIdx)
+	{
 		if (@event.IsActionPressed("click"))
 		{
-            GD.Print($"Clicked {this}");
-            steve.interactWhenClose = this;
-            //GD.Print(steve.interactWhenClose);
-            GetTree().SetInputAsHandled();
-        }
-    }
+			GD.Print($"Clicked {this}");
+			steve.interactWhenClose = this;
+			//GD.Print(steve.interactWhenClose);
+			GetTree().SetInputAsHandled();
+		}
+	}
 
-    public override void _Process(float delta)
-    {
+	public override void _Process(float delta)
+	{
 		if (steve.interactWhenClose == this && steve.Position.DistanceTo(Position) < activationDistance)
-        {
-            steve.interactWhenClose = null;
-            //GD.Print("interact");
-            interact();
-        }
-    }
+		{
+			steve.interactWhenClose = null;
+			//GD.Print("interact");
+			interact();
+		}
+	}
 
-    public Player getSteve() {
-        return steve;
-    }
+	public Player getSteve() {
+		return steve;
+	}
 
-    private void completeTask() {}
+	private void completeTask() {}
 
-    public virtual void interact() {}
+	public virtual void interact() {}
 
-    public virtual void _onMouseEntered() 
-    {
-        steve.setClickableName(name);
-    }
-    public virtual void _onMouseExited()
-    {
-        steve.setClickableName("");
-    }
+	public virtual void _onMouseEntered() 
+	{
+		steve.setClickableName(name);
+	}
+	public virtual void _onMouseExited()
+	{
+		steve.setClickableName("");
+	}
 }
